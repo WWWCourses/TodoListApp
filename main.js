b33cdd0f6c2d17070784a9096ff741c9c359e62c
WWWCourses <ivawebcourses@gmail.com>
2025-02-11 12:55:33 +0200

    Work in Lab21

--- main.js
+++ main.js
@@ -1,40 +1,85 @@
 function addTodo() {
     const todoTitle = dom.todoInput.value;
-    //change server state
-
-    // change local state if server responded ok
     const newTodo = {
         'task': todoTitle,
         'completed': false
     }
 
-    todoItems.push(newTodo);
-
+    //change server state
+    fetch(`${baseURL}/todos`,{
+        method:"POST",
+        headers:{
+            'Content-type':'Application/json'
+        },
+        body:JSON.stringify(newTodo)
+    })
+        .then(r=>{
+            if(r.ok){
+                return r.json()
+            }else{
+                throw new Error(`Server response status: ${r.status}`);
+            }
+        })
+        .then(data=>{
+            // change local state if server responded ok
+            todoItems.push(newTodo);
+        })
+        .catch(err=>console.error(`ERROR: ${err}`))
 }
 
 function toggleComplete(index) {
-    const todo = todoItems[index];
-    todo.completed =  !todo.completed;
+    console.log(`index: ${index}`);
+    const todo = todoItems.filter(todo=>todo.id===index)[0];
 
+    fetch(`${baseURL}/todos/${index}`, {
+        method:'PATCH',
+        body:JSON.stringify({'completed':!todo.completed}),
+        headers:{
+            'Content-type':'Application/json'
+        }
+    })
+        .then(r=>{
+            if(r.ok){
+                // change local state
+                todoItems.forEach(todo=>todo.id===index && (todo.completed=!todo.completed));
+            }else{
+                throw new Error(`Server response status: ${r.status}`);
+            }
+        })
+
+        .catch(err=>console.error(`ERROR: ${err}`))
 }
-function deleteTodo(index) {
-    todoItems.splice(index, 1);
 
+function deleteTodo(index) {
+    console.log(`index: ${index}`);
+    fetch(`${baseURL}/todos/${index}`, {
+        method:'DELETE',
+    })
+        .then(r=>{
+            if(r.ok){
+                // change local state
+                todoItems.splice(index, 1);
+            }else{
+                throw new Error(`Server response status: ${r.status}`);
+            }
+        })
+
+        .catch(err=>console.error(`ERROR: ${err}`))
+    // change local state
+    todoItems.splice(index, 1);
 }
 
 function renderTodos() {
     dom.todoList.innerHTML = '';
-    for (let i = 0; i < todoItems.length; i++) {
-        const todo = todoItems[i];
-
+    todoItems.forEach(todo => {
         dom.todoList.innerHTML += `
-            <li class="todo-item" data-id="${i}">
+            <li class="todo-item" data-id="${todo.id}">
                 <span class="${todo.completed?'completed':''}">${todo.task}</span>
                 <button class="complete-btn">${todo.completed?'Undo':'Complete'}</button>
                 <button class="delete-btn">Delete</button>
             </li>
         `;
-    }
+    });
 }
 
 // Get DOM elements
@@ -48,23 +93,24 @@ async function getTasks(url) {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
-        todoItems = [...data]
-        const newResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${data.id}`)
-        const userData = await response.json()
-
-        console.log(data);
+        // set local state
+        todoItems = [...data];
+        renderTodos();
     } catch (error) {
         console.error('Error:', error);
     }
 }
-// initialize state
-const todoItems;
-getTasks();
 
-renderTodos();
+const baseURL = 'http://localhost:3000'
+
+// define local state
+let todoItems;
+
+getTasks(`${baseURL}/todos`);
+
 
 dom.addTodoButton.addEventListener('click', (e)=>{
+    e.preventDefault();
     //change state
     addTodo();
     //change UI
@@ -74,13 +120,14 @@ dom.addTodoButton.addEventListener('click', (e)=>{
 
 dom.todoList.addEventListener('click', (e)=>{
     console.log(e.target)
+    e.preventDefault();
     const idx = e.target.parentElement.dataset.id;
 
     if(e.target.classList.contains('complete-btn')) {
-        todoItems[idx].completed = !todoItems[idx].completed;
+        toggleComplete(idx)
         renderTodos();
     }else if(e.target.classList.contains('delete-btn')){
-        todoItems.splice(idx,1);
+        deleteTodo(idx)
         renderTodos();
     }
 })
